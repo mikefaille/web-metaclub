c887e19835534c6b59ac3657e3d9a35fd9943366
Alexis Côté <alexiscote19@hotmail.com>
2016-11-17 21:01:40 -0500

    Cleanup and dependencies update
    - Added meteor typings installation
    - Removed all the old file related to Socially app
    - Removed old mobile files
    - Removed mobile file since their dependencies were outdated
    - Removed tortilla hooks
    - Updated meteor packages
    - Updated npm dependencies

--- src/client/imports/app/shared/index.ts
+++ src/client/imports/app/shared/index.ts
@@ -1,9 +1,5 @@
 import { DisplayNamePipe } from './display-name.pipe';
-import {RsvpPipe} from "./rsvp.pipe";
-import {DisplayMainImagePipe} from "./display-main-image.pipe";
 
 export const SHARED_DECLARATIONS: any[] = [
   DisplayNamePipe,
-  RsvpPipe,
-  DisplayMainImagePipe
 ];
